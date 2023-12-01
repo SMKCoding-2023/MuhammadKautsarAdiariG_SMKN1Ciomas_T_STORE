d4544395bc2ec8286d6e6b2708952642b621c59b
haliahmad <zerozerogan@gmail.com>
2023-12-01 12:32:27 +0700

    first

---
+++ layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+    <section>
+        <div class="h-screen text-white bg-slate-700">
+
+            <Header />
+            
+            <slot />
+            
+        </div>
+            <div class="bg-slate-900 text-white ">
+                <Footer />
+        </div>
+    </section>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+      <div class="grid h-screen place-content-center gap-3">
+      <h1 class="text-5xl text-blue-500">Welcome </h1>
+    </div>
+</template>
